bb904e776bd894db7393900430862f048ea0eed7
wabrborich@hotmail.com <wabrborich@hotmail.com>
2018-10-03 23:27:31 -0500

    New Exercises and minor corrections
    Added polynomial regression with tensorflow, also updated linear regression point space

--- Tensor Flow/tsPolyRegression.py
+++ Tensor Flow/tsPolyRegression.py
@@ -3,9 +3,10 @@ import sys
 from pygame.locals import *
 import tensorflow as tf
 import random as rd
+import numpy as np
 
 window = pygame.display.set_mode((500, 500), 0, 32)
-pygame.display.set_caption('Linear Regression')
+pygame.display.set_caption('Polynomial Regression')
 _with, _height = window.get_size()
 
 # Set up the colors
@@ -28,18 +29,18 @@ def draw_line(_start, _end, _stroke, _color):
     pygame.draw.line(window, _color, _start, _end, _stroke)
 
 
-m = tf.Variable(rd.random(), dtype='float32')
-b = tf.Variable(rd.random(), dtype='float32')
+a = tf.Variable(rd.randrange(-1, 1), dtype='float32')
+b = tf.Variable(rd.randrange(-1, 1), dtype='float32')
+c = tf.Variable(rd.randrange(-1, 1), dtype='float32')
 x_tens = tf.placeholder(dtype='float32')
 y_tens = tf.placeholder(dtype='float32')
+x_predict = tf.placeholder(dtype='float32')
 
-linear_model = m*x_tens + b
+poly_model = a*(tf.square(x_tens))+b*x_tens+c
 
+predict = a*(tf.square(x_predict))+b*x_predict+c
 
-predict = m*(tf.constant([-1, 1], dtype='float32'))+b
-
-
-square_error = tf.square(linear_model-y_tens)
+square_error = tf.square(poly_model-y_tens)
 loss = tf.reduce_sum(square_error)
 
 optimizer_function = tf.train.GradientDescentOptimizer(0.05)
@@ -52,7 +53,9 @@ sess.run(init)
 
 points_x = []
 points_y = []
-line = [-1, 1]
+curve_y = []
+curve_x = np.arange(-1, 1, 0.07).tolist()
+simulated = None
 
 while True:
     for event in pygame.event.get():
@@ -68,18 +71,21 @@ while True:
             points_y.append(maped_y)
             for i in range(550):
                 sess.run(train, {x_tens: points_x, y_tens: points_y})
-            line = sess.run(predict)
+            curve_y = sess.run(predict, {x_predict: curve_x})
             print(sess.run(loss, {x_tens: points_x, y_tens: points_y}))
+            simulated = True
     for i in range(len(points_x)):
         maped_x = translate(points_x[i], -1, 1, 0, _with)
         maped_y = translate(points_y[i], -1, 1, _height, 0)
         draw_circle((int(maped_x), int(maped_y)), 4, WHITE)
-    line_y1 = translate(line[0], -1, 1, _height, 0)
-    line_y2 = translate(line[1], -1, 1, _height, 0)
-    line_x1 = translate(-1, -1, 1, 0, _with)
-    line_x2 = translate(1, -1, 1, 0, _with)
-    if line_y1 < _height or line_y2 < _height:
-        draw_line((line_x1, line_y1), (line_x2, line_y2), 3, GREEN)
+    if simulated is True:
+        for i in range(len(curve_x)-1):
+            x_value = translate(curve_x[i], -1, 1, 0, _with)
+            y_value = translate(curve_y[i], -1, 1, _height, 0)
+            x_next = translate(curve_x[i+1], -1, 1, 0, _with)
+            y_next = translate(curve_y[i+1], -1, 1, _height, 0)
+            if y_value < _height:
+                draw_line((x_value, y_value), (x_next, y_next), 3, GREEN)
     pygame.display.update()
     pygame.time.wait(40)
     window.fill(BLACK)
